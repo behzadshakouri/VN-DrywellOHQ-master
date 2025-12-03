ac74c189f4e84d4be57b440534faa01f23acbecd
Behzad <behzad.shakouri92@gmail.com>
2025-12-03 09:41:14 -0500

    Some changes

--- main.cpp
+++ main.cpp
@@ -28,13 +28,15 @@ using namespace std;
 int main(int argc, char *argv[])
 {
 
-    bool Model_Creator = 1; // 1 or using modelcreator, and 0 for loading saved Json file; Set it for every simulation
+    bool Model_Creator = 1; // 1 for using modelcreator, and 0 for loading saved Json file; Set it for every simulation
 
     double Simulation_num = 8; // Simulation Number; Set it for every simulation
     double Simulation_days = 180; // Simulation Days for Json file [Constant]
 
-    double Simulation_start_time_0 = 43750; // Simulation Start Date for Model_Creator
-    double Simulation_end_time_0 = 43930; // Simulation Start Date for Model_Creator
+    double Simulation_start_time_0=43466; // Simulation Start Date for Model_Creator [Pacoima station]
+    double Simulation_end_time_0=45292; // Simulation End Date for Model_Creator [Pacoima station]
+    //double Simulation_start_time_0 = 43750; // Simulation Start Date for Model_Creator
+    //double Simulation_end_time_0 = 43930; // Simulation Start Date for Model_Creator
     //double Simulation_start_time_0 = 44864; // Simulation Start Date for Model_Creator [Test]
     //double Simulation_end_time_0 = 44866; // Simulation End Date for Model_Creator [Test]
 
